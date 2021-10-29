b9b3a5ed13d6f35230405c9421ab27b46a37e522
Alexander Meshcheryakov <87540821+KainN7@users.noreply.github.com>
2021-10-29 22:07:06 +0700

    Add files via upload

---
+++ 03_string.js
@@ -0,0 +1,52 @@
+// const name = 'Александр'
+// const age = 24
+//
+// function getAge() {
+//   return age
+// }
+
+// const output = 'Привет, меня зовут ' + name + ' и мой возраст: ' + age + ' года.'
+// const output = `Привет, меня зовут ${name} и мой возраст ${age < 30 ? 'А' : 'B'} года`
+// console.log(output)
+
+// const output = `
+//     <div>This is div</div>
+//     <p>this is p</p>
+// `
+// console.log(output)
+
+// const name = 'Александр'
+// console.log(name.length)
+// console.log(name.toUpperCase())
+// console.log(name.charAt(3))
+// console.log(name.indexOf('у'))
+// console.log(name.startsWith('Алекс'))
+// console.log(name.toLowerCase().startsWith('алекс'))
+// console.log(name.endsWith('др'))
+// console.log(name.repeat(3))
+// const string = '        password        '
+// console.log(string.trim())
+// console.log(string.trimLeft())
+// console.log(string.trimRight())
+//
+
+// function logPerson(s,name, age) {
+//  if (age < 0) {
+//    age = 'Ещё не родился'
+//  }
+//   return `${s[0]}${name}${s[1]}${age}${s[2]}`
+// }
+//
+// const personName = 'Александр'
+// const personName2 = 'Олег'
+// const personAge = 24
+// const personAge2 = -10
+//
+// const output = logPerson`Имя: ${personName}, Возраст: ${personAge}!`
+// const output2 = logPerson`Имя: ${personName2}, Возраст: ${personAge2}!`
+//
+//
+// console.log(output)
+// console.log(output2)
+//
+//
